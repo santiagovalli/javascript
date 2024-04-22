7e9930b5601b88bb7aa52e1a65dafbcf98342094
santiagovalli <154935242+santiagovalli@users.noreply.github.com>
2024-04-22 01:20:33 -0400

    corrección preentregable1

--- pruebas.js
+++ pruebas.js
@@ -1,38 +1,40 @@
 const productos = [
-    "NVIDIA GeForce RTX 4060",
-    "NVIDIA GeForce RTX 4070",
-    "NVIDIA GeForce RTX 4080",
-    "NVIDIA GeForce RTX 4090"
+    "nvidia geforce rtx 4060",
+    "nvidia geforce rtx 4070",
+    "nvidia geforce rtx 4080",
+    "nvidia geforce rtx 4090",
 ]
 
-// "NVIDIA GeForce RTX 4060 | $300",
-//  "NVIDIA GeForce RTX 4070 | $600",
-//     "NVIDIA GeForce RTX 4080 | $900",
-//     "NVIDIA GeForce RTX 4090 | $1500"
 
-let Bienvenida = confirm("Bienvenido a CPUMarters, las mejores tarjetas graficas del mercado a el mejor precio. \nDesea ver nuestro catalogo?")
 
-while(Bienvenida !== true){
-    alert("si desea realizar una compra por favor haga click en 'Aceptar'")
+ let Bienvenida = confirm("Bienvenido a CPUMarters, las mejores tarjetas graficas del mercado a el mejor precio. \nDesea ver nuestro catalogo?")
 
-    Bienvenida = confirm("Bienvenido a CPUMarters, las mejores tarjetas graficas del mercado a el mejor precio. \nDesea ver nuestro catalogo?")
-}
+ while(Bienvenida !== true){
+     alert("si desea realizar una compra por favor haga click en 'Aceptar'")
+
+     Bienvenida = confirm("Bienvenido a CPUMarters, las mejores tarjetas graficas del mercado a el mejor precio. \nDesea ver nuestro catalogo?")
+ }
+
 function catalogo() {
         const mostrarCatalogo = prompt("Que tarjeta de video desea compar? \n" + productos.join("\n")).toLowerCase();
+        console.log(mostrarCatalogo)
 
-        validarProducto(mostrarCatalogo)
+        validarProducto(mostrarCatalogo);
 
 }
 
-function validarProducto(){
-    const confirmacion = productos;
-    if(confirmacion){
-        alert(`felicidades usted ha comprado su producto, esperamos verlo pronto`);
-    }
-    else{
-        alert("por favor ingrese un producto valido de nuestro catalogo");
-        catalogo()
-    }
+function validarProducto(validacion){
+    const confirmacion = productos.includes(validacion);
+    console.log(confirmacion)
+
+     if(confirmacion){
+         alert(`felicidades usted ha comprado su producto, esperamos verlo pronto`);
+     }
+     
+      else{
+          alert("por favor ingrese un producto valido de nuestro catalogo");
+          catalogo()
+     }
 }
 
 catalogo()
