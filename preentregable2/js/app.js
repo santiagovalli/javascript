fef49369557e7d23322768cc5bc091a68f605ca3
Santiago Valli <santiagovalli22@gmail.com>
2024-05-07 15:43:10 -0400

    correcion de la segunda preentrega, carrito funcional

--- preentregable2/js/app.js
+++ preentregable2/js/app.js
@@ -3,9 +3,12 @@
 function agregarCarrito(producto) {
     const memoria = JSON.parse(localStorage.getItem("products"));
     console.log(memoria);
+
+    let cuenta = 0 
     if (!memoria) {
         const nuevoCoctel = nuevoProductoMemoria(producto)
         localStorage.setItem("products", JSON.stringify([nuevoCoctel]));
+        cuenta = 1;
     } else{
         const indiceProducto = memoria.findIndex(coctel => coctel.id===producto.id);
         console.log(indiceProducto)
@@ -13,13 +16,32 @@ function agregarCarrito(producto) {
         if(indiceProducto === -1){   
             memoriaNueva.push(nuevoProductoMemoria(producto));
             localStorage.setItem("products", JSON.stringify(memoriaNueva));
+            cuenta = 1;
         } else{
             memoriaNueva[indiceProducto].cantidad++;
+            cuenta = memoriaNueva[indiceProducto].cantidad;
         }
         localStorage.setItem("products", JSON.stringify(memoriaNueva));
+        
     }
     actualizarNumeroCarrito()
+    return cuenta;
 }
+
+function restarCarrito(producto){
+    const memoria = JSON.parse(localStorage.getItem("products"));
+    const indiceProducto = memoria.findIndex(coctel => coctel.id===producto.id);
+    if(memoria[indiceProducto].cantidad === 1){
+        memoria.splice(indiceProducto,1);
+        localStorage.setItem("products", JSON.stringify(memoria));
+    }else{
+        memoria[indiceProducto].cantidad--;
+    }
+    localStorage.setItem("products", JSON.stringify(memoria));
+    actualizarNumeroCarrito();
+}
+
+
 /** funcion para tomar un producto, agregarle cantidad 1 y devolverlo*/
 function nuevoProductoMemoria(producto){
     const nuevoCoctel = producto;
@@ -30,8 +52,13 @@ const cuentaCarritoElement = document.querySelector("#cuentaCarrito")
 
 function actualizarNumeroCarrito(){
     const memoria = JSON.parse(localStorage.getItem("products"));
-    const cuenta = memoria.reduce((acum, current) => acum+current.cantidad,0)
+    if(memoria && memoria.length > 0){
+   const cuenta = memoria.reduce((acum, current) => acum+current.cantidad,0)
    cuentaCarritoElement.innerText = cuenta 
+}else{
+  cuentaCarritoElement.innerText = 0;  
+}
+
 }
 /**funcion para cargar la cantidad de productos en el icono del carrito */
 actualizarNumeroCarrito()
--- preentregable2/js/cart.js
+++ preentregable2/js/cart.js
@@ -1,25 +1,83 @@
 const productsContainer = document.querySelector("#productsContainer")
+const unidadesElement = document.querySelector("#unidades")
+const precioElement = document.querySelector("#precio")
+const totalesElement = document.querySelector("#totales")
+const carritoVacioElement = document.querySelector("#carritoVacio")
+const reiniciarCarritoElement = document.querySelector("#reiniciarCarrito")
 
-function crearCardCoctel(){
+function crearCardCoctel() {
+    productsContainer.innerHTML = "";
     const productos = JSON.parse(localStorage.getItem("products"))
-    productos.forEach(product => {
-        const cardCoctel = document.createElement("div")
-        cardCoctel.setAttribute("id", "tarjetaCoctel")
 
-        cardCoctel.innerHTML = `
+    if (productos && productos.length > 0) {
+        productos.forEach((product) => {
+            const cardCoctel = document.createElement("div")
+            cardCoctel.setAttribute("id", "tarjetaCoctel")
+
+            cardCoctel.innerHTML = `
 <div id="productoCoctel">
     <img class="coctelImg" src="${product.img}" alt="${product.name}">
     <h4 class="nombreCoctel">${product.name}</h4>
     <h6 class="precioCoctel">Precio: $${product.price}</h6>
         <div class="controladorCarrito">
-            <button id="${product.id}" class="btnCarrito">-</button>
-            <span class"cantidad"=>0</span>
+            <button id="${product.id}" class="btnRestar">-</button>
+            <span class"cantidad"=>${product.cantidad}</span>
             <button id="${product.id}" class="btnSumar">+</button>
         </div>
 </div>`;
-        productsContainer.appendChild(cardCoctel);
+            productsContainer.appendChild(cardCoctel);
 
-        cardCoctel.querySelector('.btnSumar').addEventListener("click", ()=> agregarCarrito(product) )
-    })
+            
+            cardCoctel.querySelector('.btnSumar').addEventListener("click", (e) => {
+                
+                const cuentaElement = e.target.parentElement.getElementsByTagName("span")[0];
+                cuentaElement.innerText = agregarCarrito(product) ;
+                actualizarTotales();
+            });
+
+            cardCoctel.querySelector('.btnRestar').addEventListener("click", (e) => {
+                restarCarrito(product);
+                crearCardCoctel();
+                actualizarTotales();
+                
+            })
+
+        })
+
+    }
+
 }
-crearCardCoctel(products);
+crearCardCoctel();
+actualizarTotales();
+
+function actualizarTotales(){
+    const productos = JSON.parse(localStorage.getItem("products"));
+
+    let unidades = 0;
+    let precio = 0;
+    if(productos && productos.length > 0){
+        productos.forEach(producto =>{
+            unidades += producto.cantidad;
+            precio += producto.price * producto.cantidad;
+        })
+        unidadesElement.innerText = unidades;
+        precioElement.innerText = precio;
+    }
+    mensajeCarritoVacio()
+}
+
+function mensajeCarritoVacio(){
+    const productos = JSON.parse(localStorage.getItem("products"));
+    carritoVacioElement.classList.toggle("escondido", productos && productos.length > 0 );
+    totalesElement.classList.toggle("escondido", !(productos && productos.length > 0 ));
+}
+
+mensajeCarritoVacio();
+
+
+reiniciarCarritoElement.addEventListener("click",reiniciarCarrito);
+function reiniciarCarrito(){
+    localStorage.removeItem("products");
+    actualizarTotales();
+    crearCardCoctel();
+}
